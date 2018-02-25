4504711d2a6e8bd0fab5aee296dc9151ff697e6e
Carlos A. Lopez <Lope922@gmail.com>
2018-02-24 22:09:50 -0600

    Have dummy data working. About to add Entity Models

---
+++ MenuBuilderWeb/Pages/Recipies.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MenuBuilderWeb.Pages
+{
+    public class RecipiesModel : PageModel
+    {
+
+        public IEnumerable<string> Recipies { get; set; }
+        public void OnGet()
+        {
+
+            ViewData["Title"] = "Menu Build Web SIte - Recipies";
+
+            Recipies = new[]
+
+            {"Breakfast Meals", "Lunch Meals", "Dinner Meals", "Desserts", ""};
+
+
+        }
+    }
+}
